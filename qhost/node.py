bcd60c7e4786990c5c27be90aa7a414c9272a4f3
Kevin Manalo <kmanalo@gmail.com>
2015-10-29 13:01:09 -0400

    Comment cleanups, Job class not needed

--- qhost/node.py
+++ qhost/node.py
@@ -52,9 +52,6 @@ class Node:
 
     def has_user(self, userid):
         # check if any elements in userid list are in another list
-        #print userid, self.jobs
-        #import sys
-        #sys.exit(1)
         return [i for i in self.jobs if i in userid]
 
     def has_note(self):
@@ -73,37 +70,7 @@ class Node:
         compare_state = list(states)
         return set(compare_state) <= set(self.state)
 
-#class Job:
-#    def __init__(self, name):
-#        self.name = name
-#        self.users = []
-#
-#    def __repr__(self):
-#        return self.name
-#
-#    def __str__(self):
-#        return self.name
-#
-#    def from_hash(self, attrs):
-#        for attr, value in attrs.iteritems():
-#            setattr(self, attr, value)
-#
-#    def has_userid(self, userid, jobid_to_user):
-#        return userid in self.users
-#
-#    def matches(self, regex):
-#        match = re.compile(regex)
-#        return not match.search(self.name) is None
-#
-#    def state_exclusive_matches(self, states):
-#        compare_state = sorted(list(states))
-#        # compare the intersection set to the self.state set
-#        return set(self.state) & set(compare_state) == set(self.state)
-#
-#    def state_any_matches(self, states):
-#        compare_state = list(states)
-#        return set(compare_state) <= set(self.state)
 
-class Job(Node):
-    def __init__(self, name):
-        Node.__init__(self, name)
+#class Job(Node):
+#    def __init__(self, name):
+#        Node.__init__(self, name)
