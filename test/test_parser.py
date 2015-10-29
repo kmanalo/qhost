4e62b75954b2b8dd736e6fd15f87d2c42e70d72c
Kevin Manalo <kmanalo@gmail.com>
2015-10-29 13:44:30 -0400

    Parser --> Parser_Pbsnodes in Tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -3,19 +3,19 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../'))
 
-from qhost import Parser
+from qhost import Parser_Pbsnodes
 
 class TestParser(unittest.TestCase):
     def test_parse_01(self):
         output = open('output/output_01.xml', 'r').read()
-        p = Parser(output).parse()
+        p = Parser_Pbsnodes(output).parse()
         n = p[0]
         self.assertEqual(n.name, 'n061')
         self.assertEqual(len(n.jobs), 1)
 
     def test_parse_02(self):
         output = open('output/output_02.xml', 'r').read()
-        p = Parser(output).parse()
+        p = Parser_Pbsnodes(output).parse()
         n = p[0]
         self.assertEqual(n.name, 'n061')
         self.assertEqual(len(n.jobs), 1)
@@ -26,7 +26,7 @@ class TestParser(unittest.TestCase):
     def test_parse_03(self):
         # parses a node in an offline state
         output = open('output/output_03.xml', 'r').read()
-        p = Parser(output).parse()
+        p = Parser_Pbsnodes(output).parse()
         n = p[0]
         self.assertEqual(n.sessions, [])
         self.assertEqual(n.nsessions, 0)
